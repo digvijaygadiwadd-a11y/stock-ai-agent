566fbe08f1e0862c5c1cae9d8994a6cae2f55df1
DIGVIJAY GADIWADD <digvijaygadiwadd@gmail.com>
2026-06-30 19:29:29 +0530

    Implement loading of waiting signals from JSON
    Added functionality to load waiting signals from a JSON file.

--- breakout_monitor.py
+++ breakout_monitor.py
@@ -1,1 +1,18 @@
+import json
+import yfinance as yf
+from utils.telegram_sender import send_message
 
+WAITING_FILE = "waiting_signals.json"
+
+
+def load_waiting():
+    try:
+        with open(WAITING_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return {}
+
+
+waiting = load_waiting()
+
+print(f"Total Waiting Stocks : {len(waiting)}")
