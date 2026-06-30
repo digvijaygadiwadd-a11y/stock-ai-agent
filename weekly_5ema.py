6ebf1f286760536b4920ba0bbd17c6cdb18748f3
DIGVIJAY GADIWADD <digvijaygadiwadd@gmail.com>
2026-06-30 14:21:12 +0530

    Update find_signal to use nse_eq_symbols and improve error handling
    Refactor find_signal function to use NSE stock list and handle exceptions.

--- weekly_5ema.py
+++ weekly_5ema.py
@@ -1,76 +1,18 @@
-import yfinance as yf
+from strategy.weekly_strategy import find_signal
+from nsepython import nse_eq_symbols
 
+print("\nDownloading NSE Stock List...\n")
 
-def find_signal(stock):
+stocks = nse_eq_symbols()
 
-    print(f"\nScanning {stock}")
+print(f"Total Stocks : {len(stocks)}\n")
 
-    df = yf.download(
-        stock,
-        period="10y",
-        interval="1wk",
-        progress=False,
-        auto_adjust=False
-    )
+for stock in stocks:
 
-    if df.empty:
-        return
+    symbol = stock + ".NS"
 
-    high = df["High"]
-    low = df["Low"]
-    close = df["Close"]
+    try:
+        find_signal(symbol)
 
-    if hasattr(high, "columns"):
-        high = high.iloc[:, 0]
-
-    if hasattr(low, "columns"):
-        low = low.iloc[:, 0]
-
-    if hasattr(close, "columns"):
-        close = close.iloc[:, 0]
-
-    ema5 = close.ewm(span=5, adjust=False).mean()
-
-    signal_high = None
-    signal_low = None
-    signal_date = None
-
-    waiting_for_breakout = False
-
-    # Ignore the latest incomplete weekly candle
-    for i in range(len(df) - 1):
-
-        # New signal replaces old one ONLY if breakout hasn't happened
-        if high.iloc[i] < ema5.iloc[i]:
-
-            signal_high = float(high.iloc[i])
-            signal_low = float(low.iloc[i])
-            signal_date = df.index[i]
-
-            waiting_for_breakout = True
-            continue
-
-        if waiting_for_breakout:
-
-            if high.iloc[i] > signal_high:
-
-                print("\n🚀 BUY SIGNAL")
-                print("-------------------------")
-                print("Signal Date :", signal_date)
-                print("Entry       :", signal_high)
-                print("Stop Loss   :", signal_low)
-                print("Breakout On :", df.index[i])
-
-                return
-
-    if waiting_for_breakout:
-
-        print("\n⏳ Waiting For Breakout")
-        print("-------------------------")
-        print("Signal Date :", signal_date)
-        print("Entry       :", signal_high)
-        print("Stop Loss   :", signal_low)
-
-    else:
-
-        print("No Signal")
+    except Exception as e:
+        print(symbol, "FAILED")
